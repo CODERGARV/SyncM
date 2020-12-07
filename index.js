963ea747938423dcddecd22003cb4e18cc28fd85
codergarv <codergarv22@gmail.com>
2020-12-07 13:42:02 +0530

    cleaned code

--- index.js
+++ index.js
@@ -1,71 +1,60 @@
-// const { v4: uuidv4 } = require('uuid');
-// const express = require('express');
-
-// const app = express();
-// const server = require('http').Server(app);
-// const io = require('socket.io')(server);
-
-// app.set('view engine', 'ejs');
-// app.use(express.static('public'));
-
-// app.get('/', (req, res) => {
-// 	res.redirect(`/${uuidv4()}`);
-// })
-
-// app.get('/:room',(req,res) => {
-// 	res.render('room',{ roomId: req.params.room })
-// })
-
-// io.on("connection", (socket) => {
-//   socket.on("join-room", (roomId, userId) => {
-//     socket.join(roomId);
-//     socket.to(roomId).broadcast.emit("user-connected", userId);
-
-//     socket.on("message", (message) => {
-//       io.to(roomId).emit("createMessage", message);
-//     });
-//   });
-// });
-
-// // app.listen(process.env.PORT || 3000, () => {
-// // 	console.log('SERVER HAS STARTED');
-// // })
-
-// server.listen(3000);
-
 //making new
+const { v4: uuidv4 } = require('uuid');
+var bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
+// for parsing application/json
+app.use(bodyParser.json()); 
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true })); 
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/',(req,res) => {
-	res.render('room');
+	res.render('first');
 })
 
+app.post('/createRoom', function(req, res){
+  console.log(req.body);
+  console.log("hlo crete room");
+  const username=req.body.username;
+  res.redirect(`/${uuidv4()}`);
+});
 
+app.post('/joinRoom', function(req, res){
+  console.log(req.body);
+  console.log("hlo join room");
+  const username=req.body.username;
+  const roomid=req.body.roomid;
+  res.redirect(`/${roomid}`);
+});
+
+app.get('/:roomid',(req,res) => {
+  console.log("hi g g");
+	res.render('room',{ roomid: req.params.roomid});
+})
 
 io.on("connection",(socket) => {
   console.log("connected with io");
-
-  // io.sockets.emit('broadcast',{ description: ' clients connected!'});
-
-  // socket.on('play',playmsg =>{
-  //   console.log(playmsg);
-  //   // io.sockets.emit('playonclient',{song:"tu chaiye ye"});
-  // })
-//   socket.on('broadcast',function(data) {
-//     // document.body.innerHTML = '';
-//     // document.write(data.description);
-//     console.log("hlo");
-//  });
+  socket.on('joinRoom',({username,roomid})=>{
+    socket.join(roomid);
+    console.log(`${username} has joined`);
+  })
   socket.on('clientEvent', function(data) {
     console.log(data);
     io.sockets.emit('playonall',{msg:"playing on all client"});
   });
+
+  socket.on('clientEventPause', function(data) {
+    console.log(data);
+    io.sockets.emit('pauseonall',{msg:"pausing on all client"});
+  });
+
 })
 
 server.listen(3001);
